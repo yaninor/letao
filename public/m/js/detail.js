c8031fa1b5fd6160fc7372b781f260d6d0c10bac
yaninor <yaninor@qq.com>
2018-12-15 21:54:14 +0800

    乐淘项目第三天商品列表完善以及详情界面功能完成与添加到购物车

---
+++ public/m/js/detail.js
@@ -0,0 +1,91 @@
+$(function(){
+    //获取传递过来的id
+    var id =getQueryString('id');
+    // 发起请求 返回查询id的详情
+    $.ajax({
+        url:'/product/queryProductDetail',
+        data:{id:id},
+        success:function(data){
+            //根据传递过来的尺码35-45截取最大 最小值
+            var min = data.size.split('-')[0]-0;
+            var max = data.size.split('-')[1];
+            data.size = [];
+            // 遍历数组,将数组中的数字添加到尺码当中
+            for(var i=min;i<=max;i++){
+                data.size.push(i);
+            }
+            console.log(data);
+             // 4. 调用商品详情的模板生成html
+            var html = template('productDetailTpl', data);
+            $('#productDetail').html(html);
+            mui('.mui-scroll-wrapper').scroll({
+                deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+            });
+            // 等轮播图结构出来了之后再初始化轮播图
+            mui('.mui-slider').slider({
+                interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
+            });
+             //数字框也是动态添加要手动初始化
+            mui('.mui-numbox').numbox();
+            //尺码默认也是不能点击的手动初始化
+            $('.btn-size').on('tap', function() {
+                $(this).addClass('active').siblings().removeClass('active');
+            });
+        }
+    });
+    //添加到购物车点击事件
+    $('.btn-add-cart').on('tap',function(){
+         var size = $('.btn-size.active').data('size');
+        //判断如果尺码没有选择 提示用户选择尺码
+        if (!size) {
+            // toast 消息提示框 提示用户选择尺码 第一个参数 提示内容 第二个参数提示配置项
+            mui.toast('请选择尺码！', { duration: 3000, type: 'div' });
+            // 注意当用户没有选择 后面的代码不执行的要return  return false 不仅结束还可以阻止默认行为
+            return false;
+        }
+        //  获取当前选择的数量
+        var num = mui('.mui-numbox').numbox().getValue();
+        // 判断是否选择了数量
+        if (!num) {
+            mui.toast('请选择数量！', { duration: 3000, type: 'div' });
+            return false;
+        }
+        //将数据提交到服务器
+        $.ajax({
+            url:'/cart/addCart',
+            data:{productId:id,size:size,num:num},
+            type:'post',
+            success:function(data){
+                console.log(data);
+                    // 加入失败 先去完整版登录
+                    // 判断加入购物车是否成功 如果返回值success表示成功 成功提示用户去购物车查看
+                    if(data.success){
+                        // 提示用户是否去购物车查看
+                        // 第一个参数是提示的内容 第二个参数是提示的标题 第三个参数是提示按钮的文字(数组)  第四个的回调函数 当点击按钮的后会触发的回调函数
+                        mui.confirm('加入购物车成功！ 是否要去购物车查看?', 'hello 单身狗', ['去看','发呆','不看'], function(e){
+                            // 获取当前用户点击了左边的还是右边
+                            console.log(e);
+                            if(e.index == 0){
+                                //点击了左边 跳转到购物车查看
+                            }else{
+                                // 点击否就不看 表示还继续吗
+                                mui.toast('你继续加一件就可以脱离单身了！', { duration: 3000, type: 'div' });
+                            }
+                        });
+                    }
+            }
+        })
+    })
+
+         // 根据url参数名取值
+         function getQueryString(name) {
+            var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+            var r = window.location.search.substr(1).match(reg);
+        // console.log(r);
+        if (r != null) {
+            //转码方式改成 decodeURI
+            return decodeURI(r[2]);
+        }
+        return null;
+    }
+});
--- public/m/js/productList.js
+++ public/m/js/productList.js
@@ -5,25 +5,145 @@ $(function () {
         3. 创建商品列表的模板 传入数据
         4. 把模板渲染到页面*/
     // 1. 获取url中的参数的值 按照=分割取第二个值(索引是1) 进行转码
-    var search = decodeURI(location.search.split('=')[1]);
+    var search = getQueryString('search');
     // 2. 调用商品列表的APi
-    getProductList(search);
+    getProductList();
     $('.btn-search').on('tap',function(){
-        getProductList($('.input-search').val());
+        search = $('.input-search').val();
+         //搜索框内容非空判断
+         if(!search.trim()){
+            //为空弹出提示框
+            alert('请输入要搜索商品');
+            return;
+        }
+        page = 1;
+        getProductList();
     });
-    function getProductList(search){
+    function getProductList(){
         $.ajax({
             url:'/product/queryProduct',
         // page 第几页 pageSize 每页大小 proName搜索关键字
-        data:{page:1,pageSize:4,proName:search},
+        data:{page:1,pageSize:2,proName:search},
         success:function (data) {
             // 3. 调用模板生成html
             var html = template('productTpl',data);
-            console.log(data);
             // 4. 渲染到商品列表内容的ul
             $('.product-content ul').html(html);
         }
     })
     }
-
-})
+    //排序按钮添加点击事件
+    $('.product-list ul li a').on('tap',function(){
+        //获取自定义的排序类型
+        var sortType = $(this).data('sort-type');
+        //获取自定义的排序参数
+        var sort = $(this).data('sort');
+        //当参数为1的时候,点击按钮修改为2
+        if(sort == 1){
+            sort = 2;
+            // 将箭头的指向改为向上
+            $(this).find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+        }else {
+            sort = 1;
+            $(this).find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+        }
+        //点击按钮给li添加active类
+        $(this).parent().addClass('active').siblings().removeClass('active');
+        //将修改后的排序参数设置到页面属性上
+        $(this).data('sort', sort);
+        //将发送请求的参数保存到数组里,方便进行排序的类型判断
+        var params = {page:1,pageSize:2,proName:search};
+        //将排序类型根据点击的不同按钮动态添加到发送请求的参数中
+        params[sortType] = sort;
+        //发送排序请求并渲染到页面上
+        $.ajax({
+            url:'/product/queryProduct',
+        // page 第几页 pageSize 每页大小 proName搜索关键字
+        data:params,
+        success:function (data) {
+            // 3. 调用模板生成html
+            var html = template('productTpl',data);
+            // 4. 渲染到商品列表内容的ul
+            $('.product-content ul').html(html);
+        }
+    })
+    });
+    var page=1;
+    mui.init({
+        pullRefresh: {
+            container: "#refreshContainer",
+            // 初始化下拉
+            down: {
+                contentdown: "你正在进行下拉还可以继续拉...",
+                contentover: "你已经拉到了可以刷新的位置 可以松手了",
+                contentrefresh: "正在给你拼命刷新数据...",
+                callback: function() { //下拉刷新的回调函数 进行数据请求 下拉松手后就会还执行
+                    //本地速度很快 加一个定时器延迟1秒钟执行请求和结束下拉效果
+                    setTimeout(function() {
+                        // 2. 拉了之后请求数据刷新页面 发请求刷新数据即可
+                        getProductList();
+                        // 3. 当数据请求完毕页面刷新完毕后 结束下拉刷新  函数版本不一样 函数名不一样 注意当前结束函数
+                        // endPulldownToRefresh
+                        mui('#refreshContainer').pullRefresh().endPulldownToRefresh();
+                        // 9. 下拉完成后 重置这个上拉加载的效果
+                        mui('#refreshContainer').pullRefresh().refresh(true);
+                        // 10. 把page也要重置为1
+                        page = 1;
+                    }, 1000);
+                }
+            },
+            // 初始化上拉
+            up: {
+                contentrefresh: "正在拼命加载更多数据...",
+                contentnomore: '再下实在是给不了更多!',
+                callback: function() {
+                    // 请求更多数据 在页面中去追加
+                    // 如果请求更多数据 修改参数的page值 第一页是1  第二页2 第三页3
+                    setTimeout(function() {
+                        // 5. 每次上拉让当前的page++
+                        page++;
+                        // 6. 调用API传入对应的参数 请求++之后的数据
+                        $.ajax({
+                            url: '/product/queryProduct',
+                            // page 第几页使用当前++后的page变量 pageSize 每页大小 proName搜索关键字
+                            data: { page: page, pageSize: 2, proName: search },
+                            success: function(data) {
+                                // 7. 判断数据还有没有长度 有长度表示有数据 追加 没长度 表示没有数据 就结束下拉并且提示没有数据
+                                // data是对象 {data:[]}  data.data取 data对象里面的data数组 里面data数组的.length
+                                if (data.data.length > 0) {
+                                    // 7.1 调用模板生成html
+                                    var html = template('productTpl', data);
+                                    // 7.2 追加到商品列表内容的ul
+                                    $('.product-content ul').append(html);
+                                    // 7.3 加载完成后结束上拉加载的效果 MUI 结束上拉的函数endPullupToRefresh
+                                    mui('#refreshContainer').pullRefresh().endPullupToRefresh(false);
+                                }else{
+                                    // 8. 没有长度提示没有数据 了 endPullupToRefresh提示没有数据传人一个true
+                                    mui('#refreshContainer').pullRefresh().endPullupToRefresh(true);
+                                }
+                            }
+                        });
+                    }, 1000);
+                }
+            }
+        }
+    });
+    //立即购买按钮点击事件
+    $('.product-content ').on('tap','.btn-buy',function(){
+        //获取自定义的id属性
+        var id = $(this).data('id');
+        // 跳转到商品详情页面并传递当前商品的id
+        location = 'detail.html?id='+id;
+    })
+      // 根据url参数名取值
+      function getQueryString(name) {
+        var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+        var r = window.location.search.substr(1).match(reg);
+        // console.log(r);
+        if (r != null) {
+            //转码方式改成 decodeURI
+            return decodeURI(r[2]);
+        }
+        return null;
+    }
+});
